70c85b063bb7b63da99244d94fcc7cde6a088fd9
lizihan00787 <lizihan00787@gmail.com>
2025-05-09 18:42:42 +0800

    5月9日

---
+++ Luogu/DP/数位DP/P9129 [USACO23FEB] Piling Papers G.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define int long long
+#define ull unsigned long long
+#define PII pair<int,int>
+#define mk(a,b) make_pair(a,b)
+using namespace std;
+template<typename P>
+inline void read(P &x){
+    P res=0,f=1;
+    char ch=getchar();
+    while(ch<'0' || ch>'9'){
+        if(ch=='-') f=-1;
+        ch=getchar();
+    }
+    while(ch>='0' && ch<='9'){
+        res=(res<<3)+(res<<1)+(ch^48);
+        ch=getchar();
+    }
+    x=res*f;
+}
+template<typename Ty,typename ...Args>
+inline void read(Ty &x,Args &...args) {read(x);read(args...);}
+int T=1;
+
+const int N=320,M=5e4+20;
+
+int n,A,B,a[N];
+int q;
+
+
+inline void solve(){
+    read(n,A,B);
+    for(int i=1;i<=n;++i) read(a[i]);
+    read(q);
+    while(q--){
+        
+    }
+}
+
+signed main(){
+    //freopen(".in","r",stdin);
+    //freopen(".out","w",stdout);
+    //read(T);
+    while(T--) solve();
+    return 0;
+}
